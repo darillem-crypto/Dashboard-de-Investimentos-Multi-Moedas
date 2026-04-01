9c4215bf60c0cb12a76e910b0679bf684b2805d5
leticiamartins-maker <letici4martins16@gmail.com>
2026-04-01 19:13:04 -0400

    foi feito os calculos e mostrado ao usuario

--- API e Data.py
+++ API e Data.py
@@ -1,12 +1,24 @@
 
 import requests
-
-url = "GET https://economia.awesomeapi.com.br/json/last/{moedas}?token=SEU_API_KEY"
-
+moedas = "USD"
+url = f"https://economia.awesomeapi.com.br/json/last/{moedas}"
 response = requests.get(url)
-
 usd = response.json()
 
+
+for chave_moeda in usd:
+    dados = usd[chave_moeda]
+    venda= float(dados['ask'])
+    compra = float(dados['bid'])
+    preco_final= (compra + venda) /2
+    porcent_de_variação = float(dados['pctChange'])
+    abertura = compra/(1 + porcent_de_variação/100)
+    variação = ((preco_final - abertura)/abertura) *100
+    print(f'a sua moeda escolhida foi:{dados['code']}')
+    print(f'a media do preço é: {preco_final:.4f}')
+    print(f'a porcentagem de variação informada pela API é: {porcent_de_variação:.2f}%')
+    print(f'o valor estimado no inicio do dia foi de : R$ {abertura:.4f}')
+    print(f'o preço médio oscilou em relação à abertura: {variação:.2f}%')
 
 
 
